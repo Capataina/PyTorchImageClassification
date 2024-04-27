b20753cb7de15d4a4decaac4158eb81f0a3fe1a2
Capataina <ata02caner@gmail.com>
2024-04-27 15:39:24 +0100

    downloaded, transformed, reformatted and loaded the dataset for the neural network. Shuffled the training set for better learning

--- PyTorchImageClassification.py
+++ PyTorchImageClassification.py
@@ -1,8 +1,8 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torchvision import datasets, transforms
-
+import torch  # Needed for the neural networks
+import scipy  # Needed to process the downloads from torchvision
+import torch.nn as nn  # Needed for the neural networks
+import torch.optim as optim  # Needed to optimise the neural network
+from torchvision import datasets, transforms  # Needed to download and transform/process the images
 
 # Define the data transformations, this way all the images have the same size.
 # This allows us to run the images through the same neural network.
@@ -12,3 +12,17 @@ transform = transforms.Compose([
     transforms.ToTensor(),  # Convert the images to PyTorch tensors.
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Normalize the image tensors.
 ])
+
+# Load the Flowers-102 dataset from torchvision.
+# Torchvision already has the Oxford Flowers-102 dataset in it, so all we have to do is download it.
+train_dataset = datasets.Flowers102(root='./data', split='train', transform=transform, download=True)
+val_dataset = datasets.Flowers102(root='./data', split='val', transform=transform, download=True)
+test_dataset = datasets.Flowers102(root='./data', split='test', transform=transform, download=True)
+
+# Create data loaders. These allow you to load the data for the neural network. The batch size allows you to train
+# the model in batches rather than per image, allowing better and more complex learning. We shuffle the training set
+# so that in each epoch, the neural network is opposed to different orders of images, strengthening the hidden layers.
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
+val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=32, shuffle=False)
+test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=32, shuffle=False)
+
